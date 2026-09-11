74409e90a89db982c9e5a374810ff1cb4f009e1b
kei <2600678@sit.singaporetech.edu.sg>
2026-09-11 15:43:09 +0800

    finally done

--- auditor.py
+++ auditor.py
@@ -1,1 +1,34 @@
-print("Hello World!")
+#init
+inventory = 0
+noFE = 0
+
+#loop
+while True:
+     #check if inv exceed 500 units, break if true
+    if inventory >= 500:
+        break
+
+    #input
+    stockQty = input("Enter the stock quantity (or type 'quit' to quit): ")
+
+    #check if quit
+    if stockQty.lower() == 'quit':
+        print("Total Units Processed: ", inventory)
+        print("Number of Failed/Rejected Entries: ", noFE)
+        break
+
+    #check -ve num
+    if stockQty.startswith("-") and stockQty[1:].isdigit():
+        print("Stock quantity cannot be negative.")
+        noFE += 1
+        continue
+    #check non-digit
+    elif not stockQty.isdigit():
+        print("Please enter a valid number for stock quantity.")
+        noFE += 1
+        continue
+    #else add to inv
+    else:
+        stockQty = int(stockQty)
+        inventory += stockQty
+        print(f"Current inventory: {inventory}")
